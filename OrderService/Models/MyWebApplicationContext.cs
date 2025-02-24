9f7b4723a99e73aa65fb3aa7afa9f1342e27213b
Victor Chang <victor.chang@hotmail.com>
2025-02-24 11:52:28 -0700

    remove db connection from DbContext

--- OrderService/Models/MyWebApplicationContext.cs
+++ OrderService/Models/MyWebApplicationContext.cs
@@ -17,9 +17,6 @@ public partial class MyWebApplicationContext : DbContext
 
     public virtual DbSet<Order> Orders { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MyWebApplication;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Encrypt=True;Trust Server Certificate=False;Command Timeout=0");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
